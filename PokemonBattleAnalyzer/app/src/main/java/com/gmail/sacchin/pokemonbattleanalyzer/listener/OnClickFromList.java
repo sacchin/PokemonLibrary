e26392369140acc2d7a7b04aee54e4c9d7125be8
satonaka yusuke <sacchin13@gmail.com>
2015-07-07 00:24:31 +0900

    見せ合いの機能を一部実装

--- PokemonBattleAnalyzer/app/src/main/java/com/gmail/sacchin/pokemonbattleanalyzer/listener/OnClickFromList.java
+++ PokemonBattleAnalyzer/app/src/main/java/com/gmail/sacchin/pokemonbattleanalyzer/listener/OnClickFromList.java
@@ -1,25 +1,22 @@
 package com.gmail.sacchin.pokemonbattleanalyzer.listener;
 
-import com.gmail.sacchin.pokemonbattleanalyzer.fragment.MainFragment;
+import com.gmail.sacchin.pokemonbattleanalyzer.AddToListInterface;
 import com.gmail.sacchin.pokemonbattleanalyzer.entity.PBAPokemon;
 
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class OnClickFromList implements OnClickListener{
 	private PBAPokemon pokemon = null;
-	private MainFragment fragment = null;
+	private AddToListInterface someFragment = null;
 
-	public OnClickFromList(MainFragment fragment, PBAPokemon pokemon){
+	public OnClickFromList(AddToListInterface someFragment, PBAPokemon pokemon){
 		this.pokemon = pokemon;
-		this.fragment = fragment;
+		this.someFragment = someFragment;
 	}
 
 	@Override
 	public void onClick(View v) {
-        fragment.addPokemonToList(pokemon);
+		someFragment.addPokemonToList(pokemon);
 	}
-
 }
